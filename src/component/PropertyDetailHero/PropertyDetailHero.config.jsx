34ddd87707d7e98ceeb3489bd0cec6515335edea
BearingMe <gomes.bruno201@gmail.com>
2024-02-01 17:41:15 -0300

    feat: create tiny slider carousel config

---
+++ src/component/PropertyDetailHero/PropertyDetailHero.config.jsx
@@ -0,0 +1,38 @@
+/**
+ * Calculate the number of items to show in the slider
+ * @param {number} length - actual number of images
+ * @param {number} target - target number of images to show
+ * @returns 
+ */
+function calcItemsSpace(length, target) {
+  if (length >= target) return target;    
+  return length + 1;
+};
+
+/**
+ * Create carousel settings
+ * @param {{src: string, position: number}[]} images - array of image objects
+ * @returns {object} 
+ */
+function createCarouselSettings(images) {
+  return {
+    nav: false, // show navigation dots
+    loop: false, // disable loop
+    mouseDrag: true, // enable drag image to move around
+    items: 1, // show 1 image at a time by default
+    gutter: 10, // space between images
+    controls: false, // hide control buttons (prev/next)
+    responsive: {
+      768: {
+        items: calcItemsSpace(images.length, 2)
+      },
+      1200: {
+        items: calcItemsSpace(images.length, 3),
+
+      },
+    }
+  };
+
+}
+
+export { createCarouselSettings };
