6067161913bc1bb12616cc31fe3b6b307f65922f
BearingMe <gomes.bruno201@gmail.com>
2024-02-06 16:23:24 -0300

    feat: change slide controller button layout

--- src/component/PropertyDetailHero/PropertyDetailHero.root.jsx
+++ src/component/PropertyDetailHero/PropertyDetailHero.root.jsx
@@ -49,13 +49,19 @@ function Root({ images, onClick }) {
         className="absolute top-1/2 -translate-y-1/2 left-0 right-0 flex justify-between pb-10 px-4 pointer-events-none"
       >
         {/* left button  */}
-        <button id="tns-prev-button" className="btn btn-icon">
-          <i className="uil uil-angle-left-b text-5xl text-slate-900/60 hover:text-yellow-400 pointer-events-auto"></i>
+        <button 
+          id="tns-prev-button" 
+          className="btn btn-icon rounded-full bg-yellow-400 hover:bg-yellow-500 p-4 pointer-events-auto"
+        >
+          <i className="uil uil-angle-left-b text-3xl text-slate-900/60 flex justify-center pr-[2px]"></i>
         </button>
         
         {/* right button */}
-        <button id="tns-next-button" className="btn btn-icon">
-          <i className="uil uil-angle-right-b text-5xl text-slate-900/60 hover:text-yellow-400 pointer-events-auto"></i>
+        <button 
+          id="tns-next-button" 
+          className="btn btn-icon rounded-full bg-yellow-400 hover:bg-yellow-500 p-4 pointer-events-auto"
+        >
+          <i className="uil uil-angle-right-b text-3xl text-slate-900/60 flex justify-center pl-[2px]"></i>
         </button>
       </div>
     </div>
